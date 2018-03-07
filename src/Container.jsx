ab26298507a4bd4c7f4e52c0f99d723aae90c92b
Gabor Csizmadia <gcsizmadia@chemaxon.com>
2018-03-07 13:38:57 +0100

    authentication
    Adds authentication using Auth0 and an app bar that shows the logged in
    user and allows logging out.
    Also includes the removal of some Synergy-related stuff.

--- src/Container.jsx
+++ src/Container.jsx
@@ -4,6 +4,14 @@ import {
     BrowserRouter,
     Link
   } from 'react-router-dom';
+import Reboot from 'material-ui/Reboot';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+import Avatar from 'material-ui/Avatar';
+import Popover from 'material-ui/Popover';
+import Button from 'material-ui/Button';
 import StudysetPage from './StudysetPage';
 import StudysetListPage from './StudysetListPage';
 
@@ -12,30 +20,89 @@ export default class Container extends React.Component {
     constructor() {
         super();
         this.state = {
-            studysets: []
+            studysets: [],
+            profilePopoverOpen: false,
+            profilePopoverAnchorEl: null
         }
     }
 
     componentDidMount() {
-        fetch('/studysets')
+        fetch('/studysets', { credentials: 'include' })
             .then(res => res.json())
             .then(studysets => this.setState({ studysets}))
             .catch(e => console.log(e));
         console.log("study sets: ", this.state.studysets);
     }
 
+    openProfilePopover(event) {
+        this.setState({
+            profilePopoverOpen: true,
+            profilePopoverAnchorEl: event.currentTarget
+        });
+    }
+
+    closeProfilePopover() {
+        this.setState({
+            profilePopoverOpen: false
+        });
+    }
+
     render() {
         return (
             <BrowserRouter>
-                    <div>
+                <div>
+                    <Reboot />
                     <Route exact path="/" render={(props) => (
-                        <StudysetListPage {...props} studysets={this.state.studysets}/>
+                        <React.Fragment>
+                            {this.renderAppBar()}
+                            <StudysetListPage {...props} studysets={this.state.studysets}/>
+                        </React.Fragment>
                     )}/>
                     <Route path="/studyset" render={(props) => (
                         <StudysetPage {...props} studysets={this.state.studysets}/>
                     )}/>
-                    </div>
+                </div>
             </BrowserRouter>
+        );
+    }
+
+    renderAppBar() {
+        return (
+            <AppBar position="static">
+                <Toolbar style={{ justifyContent: 'space-between' }}>
+                    <Typography variant="title" color="inherit">Study sets</Typography>
+                    <IconButton onClick={this.openProfilePopover.bind(this)}><Avatar src={window.user.picture} /></IconButton>
+                    {this.renderProfilePopover()}
+                </Toolbar>
+            </AppBar>
+        );
+    }
+
+    renderProfilePopover() {
+        let { displayName, picture } = window.user;
+        return (
+            <Popover
+                open={this.state.profilePopoverOpen}
+                anchorEl={this.state.profilePopoverAnchorEl}
+                onClose={this.closeProfilePopover.bind(this)}>
+                <div style={{ display: 'flex', flexDirection: 'row', margin: 8 }}>
+                    <Avatar src={picture} style={{ width: 96, height: 96, margin: 8 }} />
+                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+                        <div style={{ margin: 16 }}>
+                            <Typography variant="body2">{displayName}</Typography>
+                            {this.renderEmailIfNeeded()}
+                        </div>
+                        <Button component="a" href="/logout" color="primary" style={{ alignSelf: 'flex-end' }}>Log out</Button>
+                    </div>
+                </div>
+            </Popover>
+        );
+    }
+
+    renderEmailIfNeeded() {
+        let { displayName, emails } = window.user;
+        return emails && emails.length > 0 && emails[0].value !== displayName && (
+            <Typography variant="body1">{emails[0].value}</Typography>
         );
     }
 
